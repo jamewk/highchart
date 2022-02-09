3f13c97f9d86d5a705ad428949ecf3cd878bc675
Anuwat Rodmoung <jamewk01@gmail.com>
2022-02-09 15:35:23 +0700

    Add high chart js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HighchartsChartModule } from 'highcharts-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { 
+  OutputGraphComponent 
+} from './pages/output-graph/output-graph.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OutputGraphComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HighchartsChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/output-graph/output-graph.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit} from "@angular/core";
+import * as Highcharts from "highcharts/highstock";
+import { Options } from "highcharts/highstock";
+
+import IndicatorsCore from "highcharts/indicators/indicators";
+import IndicatorZigzag from "highcharts/indicators/zigzag";
+IndicatorsCore(Highcharts);
+IndicatorZigzag(Highcharts);
+
+
+@Component({
+  selector: 'app-output-graph',
+  templateUrl: './output-graph.component.html',
+  styleUrls: ['./output-graph.component.css']
+})
+export class OutputGraphComponent implements OnInit {
+
+  Highcharts: typeof Highcharts = Highcharts;
+
+  ngOnInit() {
+    console.log(window);
+  }
+
+  chartOptions: Options = {
+    title: {
+      text: 'Average Monthly Weather Data',
+      align: 'left'
+    },
+    subtitle: {
+        text: 'Source: abc.com',
+        align: 'left'
+    },
+    yAxis: [{ // Primary yAxis
+      labels: {
+        format: '{value}°C',
+
+      },
+      title: {
+        text: 'Temperature',
+      },
+      opposite:true
+    }, { // Secondary yAxis
+      title: {
+        text: 'Rainfall',
+      },
+      labels: {
+        format: '{value} mm',
+      },
+      opposite:false
+    }],
+    tooltip: {
+      shared: true
+    },
+    series: [{
+      name: 'Rainfall',
+      type: 'spline',
+      yAxis: 1,
+      data: [[1581345000000, 49.9], [1581431400000, 71.5], [1581517800000, 106.4], [1581604200000, 129.2], [1581690600000, 144.0], [1582036200000, 176.0], [1582122600000, 135.6], [1582209000000, 148.5], [1582295400000, 216.4], [1582554600000, 194.1], [1582641000000, 95.6], [1582727400000, 54.4]],
+      tooltip: {
+        valueSuffix: ' mm'
+      }
+  
+    }, {
+      name: 'Temperature',
+      type: 'spline',
+      data: [[1581345000000,7.0], [1581431400000,6.9], [1581517800000, 9.5], [1581604200000, 14.5], [1581690600000, 18.2], [1582036200000, 21.5], [1582122600000, 25.2], [1582209000000, 26.5], [1582295400000, 23.3], [1582554600000, 18.3], [1582641000000, 13.9], [1582727400000, 9.6]],
+      tooltip: {
+        valueSuffix: '°C'
+      }
+    }]
+  };
+
+}

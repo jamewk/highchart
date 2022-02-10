554583256135f9b4fdc3931dd93b9131aa2a48fa
Anuwat Rodmoung <jamewk01@gmail.com>
2022-02-10 11:40:08 +0700

    Add select option form

--- src/app/pages/output-graph/output-graph.component.ts
+++ src/app/pages/output-graph/output-graph.component.ts
@@ -17,7 +17,7 @@ IndicatorZigzag(Highcharts);
 export class OutputGraphComponent implements OnInit {
   form: FormGroup;
   Highcharts: typeof Highcharts = Highcharts;
-
+  filter: boolean = false;
 
   chartOptions: Options = {
     title: {
@@ -28,44 +28,11 @@ export class OutputGraphComponent implements OnInit {
         text: 'Source: abc.com',
         align: 'left'
     },
-    yAxis: [{ // Primary yAxis
-      labels: {
-        format: '{value}°C',
-
-      },
-      title: {
-        text: 'Temperature',
-      },
-      opposite:true
-    }, { // Secondary yAxis
-      title: {
-        text: 'Rainfall',
-      },
-      labels: {
-        format: '{value} mm',
-      },
-      opposite:false
-    }],
+    yAxis: [],
     tooltip: {
       shared: true
     },
-    series: [{
-      name: 'Rainfall',
-      type: 'spline',
-      yAxis: 1,
-      data: [[1581345000000, 49.9], [1581431400000, 71.5], [1581517800000, 106.4], [1581604200000, 129.2], [1581690600000, 144.0], [1582036200000, 176.0], [1582122600000, 135.6], [1582209000000, 148.5], [1582295400000, 216.4], [1582554600000, 194.1], [1582641000000, 95.6], [1582727400000, 54.4]],
-      tooltip: {
-        valueSuffix: ' mm'
-      }
-  
-    }, {
-      name: 'Temperature',
-      type: 'spline',
-      data: [[1581345000000,7.0], [1581431400000,6.9], [1581517800000, 9.5], [1581604200000, 14.5], [1581690600000, 18.2], [1582036200000, 21.5], [1582122600000, 25.2], [1582209000000, 26.5], [1582295400000, 23.3], [1582554600000, 18.3], [1582641000000, 13.9], [1582727400000, 9.6]],
-      tooltip: {
-        valueSuffix: '°C'
-      }
-    }]
+    series: []
   };
 
   constructor(
@@ -75,5 +42,143 @@ export class OutputGraphComponent implements OnInit {
 
   ngOnInit() {
     console.log(window);
+
+    this.createForm();
+
+    this.setDataChart();
+
+    this.form.controls['senser_1'].valueChanges.subscribe(
+      (value) => {
+        this.setDataChart();
+      }
+    );
+
+    this.form.controls['senser_2'].valueChanges.subscribe(
+      (value) => {
+        this.setDataChart();
+      }
+    );
+  }
+
+  createForm(){
+    this.form = this.formBuilder.group({
+      senser_1: [""],
+      senser_2: [""],
+    })
+  }
+
+  setDataChart(){
+    this.filter = false;
+
+    let data = this.form.getRawValue();
+    let temperature = {
+      yAxis: { 
+          labels: {
+            format: '{value}°C',
+    
+          },
+          title: {
+            text: 'Temperature',
+          },
+      },
+      series: {
+          name: 'Temperature',
+          type: 'spline',
+          data: [[1581345000000,7.0], [1581431400000,6.9], [1581517800000, 9.5], [1581604200000, 14.5], [1581690600000, 18.2], [1582036200000, 21.5], [1582122600000, 25.2], [1582209000000, 26.5], [1582295400000, 23.3], [1582554600000, 18.3], [1582641000000, 13.9], [1582727400000, 9.6]],
+          tooltip: {
+            valueSuffix: '°C'
+          }
+      }
+    }
+
+    let humidity = {
+      yAxis: { 
+          labels: {
+            format: '{value} %',
+    
+          },
+          title: {
+            text: 'Humidity',
+          },
+      },
+      series: {
+          name: 'Humidity',
+          type: 'spline',
+          data: [[1581345000000, 49.9], [1581431400000, 71.5], [1581517800000, 106.4], [1581604200000, 129.2], [1581690600000, 144.0], [1582036200000, 176.0], [1582122600000, 135.6], [1582209000000, 148.5], [1582295400000, 216.4], [1582554600000, 194.1], [1582641000000, 95.6], [1582727400000, 54.4]],
+          tooltip: {
+            valueSuffix: '%'
+          }
+      }
+    }
+    let yAxis = [];
+    let series = [];
+    if(data.senser_1){
+ 
+      if(data.senser_1 == 'Humidity'){
+        yAxis.push(
+          {
+            ...humidity.yAxis,
+            opposite:false
+          }
+        )
+        series.push(
+          {
+            ...humidity.series,
+          }
+        )
+      }else{
+
+        yAxis.push(
+          {
+            ...temperature.yAxis,
+            opposite:false
+          }
+        )
+        series.push(
+          {
+            ...temperature.series,
+          }
+        )
+      }
+    }
+
+    if(data.senser_2){
+ 
+      if(data.senser_2 == 'Temperature'){
+        yAxis.push(
+          {
+            ...temperature.yAxis,
+
+          }
+        )
+        series.push(
+          {
+            ...temperature.series,
+          }
+        )
+      }else{
+
+        yAxis.push(
+          {
+            ...humidity.yAxis,
+          }
+        )
+        series.push(
+          {
+            ...humidity.series,
+          }
+        )
+      }
+    }
+
+    this.chartOptions.yAxis = yAxis;
+    this.chartOptions.series = series;
+
+
+    if(yAxis.length > 0){
+      setTimeout(() => {
+        this.filter = true;
+      }, 100);
+    }
   }
 }

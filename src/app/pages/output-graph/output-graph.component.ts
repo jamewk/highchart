4d3b842ce5c28d960d3d9b41b3844db2ed623b64
Anuwat Rodmoung <jamewk01@gmail.com>
2022-02-11 13:14:24 +0700

    Change senser

--- src/app/pages/output-graph/output-graph.component.ts
+++ src/app/pages/output-graph/output-graph.component.ts
@@ -95,24 +95,24 @@ export class OutputGraphComponent implements OnInit {
       yAxis: { 
         gridLineWidth: 0,
         title: {
-            text: 'Main Power',
+            text: 'Humidity',
             style: {
                 color: Highcharts.getOptions().colors[0]
             }
         },
         labels: {
-            format: '{value} volts',
+            format: '{value} %',
             style: {
                 color: Highcharts.getOptions().colors[0]
             }
         }
       },
       series: {
-          name: 'Main Power',
+          name: 'Humidity',
           type: 'spline',
           data: [[1581345000000, 20.9], [1581431400000, 71.5], [1581517800000, 106.4], [1581604200000, 129.2], [1581690600000, 144.0], [1582036200000, 176.0], [1582122600000, 135.6], [1582209000000, 148.5], [1582295400000, 216.4], [1582554600000, 194.1], [1582641000000, 95.6], [1582727400000, 54.4]],
           tooltip: {
-            valueSuffix: ' volts'
+            valueSuffix: ' %'
           }
       }
     }
